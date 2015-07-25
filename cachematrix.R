ca08de35d462f8ef601ffe145868f42c8cc4b150
Giselitaj <gisejazmin@gmail.com>
2015-07-25 10:36:10 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,12 @@
 ##Estas son un par de funciones de la Cache de un matriz que nos ayuda a evitar calculos costosos
 
-
 ##La funcion makeCacheMatrix crea una "matriz" que puede almacenar en cache su inversa
 
 ##makeCacheMatrix nos ayuda a realizar:
 
-
 ##1. Establecer el valor de una matriz 'x'
 ##2. Obtiene el valor de la matriz 'x'
 ##3. Calcula la inversa de la matriz 'x'
-
 
 makeCacheMatrix<-function(x=matrix())
 {
@@ -24,8 +21,6 @@ makeCacheMatrix<-function(x=matrix())
   getinversa<-function()m
   list(set=set,get=get,setinversa=setinvera,getinversa=getinversa)
 }
-
-
 
 ##cacheSolve es una funcion que nos ayuda a calcular la inversa de la matriz devuelta por la funcion makeCacheMatrix; si la inversa ya ha sido calculada( y la matriz no cambia), entonces la funcion cacheSolve debe recuperar la inversa de la cache
 
